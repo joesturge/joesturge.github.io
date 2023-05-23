300b88fb142ab1f2c1d64e0ffd14e12496895f28
joesturge <joe.mike.sturge@gmail.com>
2023-05-23 23:27:59 +0100

    added fractal to appbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,59 @@
-import { AppBar, Box, Toolbar, Typography, styled } from "@mui/material";
-import background from "./background.png";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  decomposeColor,
+  useTheme,
+} from "@mui/material";
 import Socials from "../Socials";
-
-const StyledToolbar = styled(Toolbar)(() => ({
-  backgroundImage: `url(${background})`,
-  backgroundRepeat: "no-repeat",
-  backgroundSize: "cover",
-}));
+import { createRef, useEffect } from "react";
+import JuliaSet from "julia-set";
 
 type HeaderProps = {
-    title: string
-}
+  title: string;
+};
 
-const Header = (props: HeaderProps) => (
-  <AppBar position="sticky">
-    <StyledToolbar>
-      <Typography>{props.title}</Typography>
-      <Box sx={{ flexGrow: 1 }} />
-      <Socials/>
-    </StyledToolbar>
-  </AppBar>
-);
+const Header = (props: HeaderProps) => {
+  const canvas = createRef<HTMLCanvasElement>();
+
+  const theme = useTheme();
+
+  useEffect(() => {
+    if (canvas.current) {
+      JuliaSet.render(canvas.current, {
+        code: "z * z + -0.72i-0.29",
+        palette: [
+          decomposeColor(theme.palette.background.default).values,
+          decomposeColor(theme.palette.primary.main).values,
+        ],
+        height: 1,
+
+        center: [0, 0],
+        iterations: 50,
+        runawayDistance: 10,
+      });
+    }
+  }, [canvas, theme.palette.background.default, theme.palette.primary.main]);
+
+  return (
+    <AppBar position="sticky">
+      <canvas
+        ref={canvas}
+        style={{
+          height: "100%",
+          width: "100%",
+          position: "absolute",
+          zIndex: -1,
+        }}
+      ></canvas>
+      <Toolbar>
+        <Typography>{props.title}</Typography>
+        <Box sx={{ flexGrow: 1 }} />
+        <Socials />
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
