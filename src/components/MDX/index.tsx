4856580d881e3efad15a1647034bd335612242c2
joesturge <joe.mike.sturge@gmail.com>
2023-06-15 10:24:16 +0000

    updated mdx components

--- src/components/MDX/index.tsx
+++ src/components/MDX/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // Copyright 2019-present ViPro <VdustR@gmail.com> (https://VdustR.github.io)
-import { memo } from "react";
 import { Typography, Paper, Divider } from "@mui/material";
 import { MDXComponents } from "mdx/types.js";
 
@@ -9,77 +8,77 @@ const components: MDXComponents = {
     const P = (props: any) => (
       <Typography {...props} />
     );
-    return memo(P);
+    return P;
   })(),
   h1: (() => {
     const H1 = (props: any) => (
       <Typography {...props} component="h1" variant="h1" />
     );
-    return memo(H1);
+    return H1;
   })(),
   h2: (() => {
     const H2 = (props: any) => (
       <Typography {...props} component="h2" variant="h2" />
     );
-    return memo(H2);
+    return H2;
   })(),
   h3: (() => {
     const H3 = (props: any) => (
       <Typography {...props} component="h3" variant="h3" />
     );
-    return memo(H3);
+    return H3;
   })(),
   h4: (() => {
     const H4 = (props: any) => (
       <Typography {...props} component="h4" variant="h4" />
     );
-    return memo(H4);
+    return H4;
   })(),
   h5: (() => {
     const H5 = (props: any) => (
       <Typography {...props} component="h5" variant="h5" />
     );
-    return memo(H5);
+    return H5;
   })(),
   h6: (() => {
     const H6 = (props: any) => (
       <Typography {...props} component="h6" variant="h6" />
     );
-    return memo(H6);
+    return H6;
   })(),
   blockquote: (() => {
     const Blockquote = (props: any) => (
       <Paper style={{ borderLeft: "4px solid grey", padding: 8 }} {...props} />
     );
-    return memo(Blockquote);
+    return (Blockquote);
   })(),
   ul: (() => {
     const Ul = (props: any) => <Typography {...props} component="ul" />;
-    return memo(Ul);
+    return Ul;
   })(),
   ol: (() => {
     const Ol = (props: any) => <Typography {...props} component="ol" />;
-    return memo(Ol);
+    return Ol;
   })(),
   li: (() => {
     const Li = (props: any) => <Typography {...props} component="li" />;
-    return memo(Li);
+    return Li;
   })(),
   hr: (() => {
     const Hr = (props: any) => <Divider {...props} component="hr" />;
-    return memo(Hr);
+    return (Hr);
   })(),
   em: (() => {
     const Em = (props: any) => (
       <Typography {...props} color="secondary" component="span" />
     );
-    return memo(Em);
+    return Em;
   })(),
   wrapper: (() => {
     const Wrapper = (props: any) => (
       <div {...props} className="markdown-body" />
     );
-    return memo(Wrapper);
+    return Wrapper;
   })(),
 };
 
